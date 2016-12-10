c5bbd0544e7d84f38e8093330cef99b775b43f1b
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-10 14:56:59 -0400

    Objects are pickupable now

---
+++ LudumDare37/Assets/Scripts/GenericPickupableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericPickupableObject : MonoBehaviour, IPickupable {
+
+	Collider collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<Collider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pickup (GameObject parent) {
+		SetParent (parent);
+		collider.attachedRigidbody.useGravity = false;
+		collider.attachedRigidbody.isKinematic = true;
+	}
+
+	public void Release(GameObject parent) {
+		SetParent (parent);
+		collider.attachedRigidbody.useGravity = true;
+		collider.attachedRigidbody.isKinematic = false;
+	}
+
+	private void SetParent(GameObject parent) {
+		if (parent != null) {
+			this.transform.parent = parent.transform;
+		} else {
+			this.transform.parent = null;
+		}
+	}
+}
--- LudumDare37/Assets/Scripts/PlayerLineOfSight.cs
+++ LudumDare37/Assets/Scripts/PlayerLineOfSight.cs
@@ -6,25 +6,45 @@ public class PlayerLineOfSight : MonoBehaviour {
 
 	public float interactionDistance;
 	RaycastHit objectInRange;
-	bool objectInHand;
+	bool isObjectInRange;
+	GameObject objectInHand;
 
 	// Use this for initialization
 	void Start () {
-		objectInHand = false;
+		isObjectInRange = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawRay (this.transform.position, this.transform.forward * interactionDistance, Color.magenta);
 
-		if (Physics.Raycast(this.transform.position, this.transform.forward, out objectInRange, interactionDistance)) {
+		if (Physics.Raycast (this.transform.position, this.transform.forward, out objectInRange, interactionDistance)) {
 //			Debug.Log ("Object in range: " + objectInRange.collider.name);
-			if (Input.GetMouseButtonDown(0)) {
-				IInteractable interactingWithObject = (IInteractable) objectInRange.collider.gameObject.GetComponent(typeof(IInteractable));
+			isObjectInRange = true;
+			if (Input.GetMouseButtonDown (0)) {
+				IInteractable interactingWithObject = (IInteractable)objectInRange.collider.gameObject.GetComponent (typeof(IInteractable));
 				if (interactingWithObject != null) {
 					interactingWithObject.Interact ();
 				}
 			}
+
+		} else {
+			isObjectInRange = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.E)) {
+			if (objectInHand != null) {
+				((IPickupable) objectInHand.GetComponent<Collider>().gameObject.GetComponent(typeof(IPickupable))).Release (null);
+				objectInHand = null;
+				Debug.Log ("Release");
+			} else if (isObjectInRange) {
+				IPickupable interactingWithObject = (IPickupable) objectInRange.collider.gameObject.GetComponent(typeof(IPickupable));
+				if (interactingWithObject != null) {
+					interactingWithObject.Pickup (this.gameObject);
+					objectInHand = objectInRange.collider.gameObject;
+					Debug.Log ("Pickup");
+				}
+			} 
 		}
 	}
 }
