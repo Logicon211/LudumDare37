8e6327b8b6da17a4e728e2bbde1bf6b6727ab79a
KingOfBraves <chadifferus@gmail.com>
2016-12-10 14:39:04 -0400

    New stuff for enemy spawn

---
+++ LudumDare37/Assets/Scripts/Enemy Spawn/EnemySpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public int maxEnemies;
+
+    EnemySpawn[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoints = GetComponentsInChildren<EnemySpawn>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
