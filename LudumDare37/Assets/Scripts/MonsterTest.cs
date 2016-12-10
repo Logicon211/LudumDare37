a623bb27c5c97554dac0dfc35a45ffa5aae21b64
KingOfBraves <chadifferus@gmail.com>
2016-12-10 00:02:57 -0400

    Created a skeleton that switches through animations

---
+++ LudumDare37/Assets/Scripts/MonsterTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class MonsterTest : MonoBehaviour {
+
+    Animator ani;
+
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("Check it");
+            ani.SetBool("Attack", true);
+        }
+	}
+}
