35506aed6d483cda72bf4988eca1cefbff61df8b
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-10 18:10:40 -0400

    Added nuke task controller

---
+++ LudumDare37/Assets/Scripts/NukeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeController : GenericPickupableObject {
+
+	private NukeTaskController nukeTaskController;
+
+	// Use this for initialization
+	void Start () {
+		nukeTaskController = GameObject.FindObjectOfType<NukeTaskController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Destroy () {
+		base.Destroy ();
+		nukeTaskController.decrementNumberOfNukes ();
+	}
+}
---
+++ LudumDare37/Assets/Scripts/NukeTaskController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeTaskController : MonoBehaviour {
+
+	private TaskController parentController;
+	private int numberOfNukes;
+	private bool taskComplete = false;
+
+	// Use this for initialization
+	void Start () {
+		numberOfNukes = GameObject.FindGameObjectsWithTag ("Nuke").Length;
+		parentController = GameObject.FindObjectOfType<TaskController> ();
+		checkTaskComplete ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void decrementNumberOfNukes () {
+		if (numberOfNukes != 0) {
+			numberOfNukes--;
+			Debug.Log ("Decremented, new number of nukes: " + numberOfNukes);
+			checkTaskComplete ();
+		}
+	}
+
+	private void checkTaskComplete () {
+		if (numberOfNukes == 0 && !taskComplete) {
+			taskComplete = true;
+			Debug.Log ("Nuke task complete");
+			parentController.triggerNukeTaskComplete ();
+		}
+	}
+}
