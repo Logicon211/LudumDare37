035057ba3c3a4cae2bd1fddced91ad5296236153
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-10 12:02:33 -0400

    Added skybox to camera

---
+++ LudumDare37/Assets/Scripts/PlayerLineOfSight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLineOfSight : MonoBehaviour {
+
+	public float interactionDistance;
+	RaycastHit objectInRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (this.transform.position, this.transform.forward * interactionDistance, Color.magenta);
+
+		if (Physics.Raycast(this.transform.position, this.transform.forward, out objectInRange, interactionDistance)) {
+			Debug.Log ("Object in range: " + objectInRange.collider.name);
+		}
+	}
+}
