dad24c24080581daf38844a59707c11cb08f56c5
KingOfBraves <chadifferus@gmail.com>
2016-12-10 01:08:03 -0400

    Base Skeleton AI

--- LudumDare37/Assets/Scripts/MonsterTest.cs
+++ LudumDare37/Assets/Scripts/MonsterTest.cs
@@ -5,19 +5,48 @@ using UnityEditor.Animations;
 
 public class MonsterTest : MonoBehaviour {
 
-    Animator ani;
+    Animator ani;   // Animation component
+    Transform plTransform;
+
+    public GameObject player;
+    public float speed;
+
+    float distance;
+    bool walking = false;
+
+    float ATTACK_RANGE = 0.3f;
 
 	// Use this for initialization
 	void Start () {
         ani = GetComponent<Animator>();
+        plTransform = player.GetComponent<Transform>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.A))
+        distance = Vector3.Distance(transform.position, plTransform.position);
+
+        // Checking to see if up arrow is up or down, will determine if the walk animation is to be played
+        if (distance > ATTACK_RANGE) { 
+            ani.SetBool("Walk", true);
+            walking = true;
+        }
+        else
         {
-            print("Check it");
+            ani.SetBool("Walk", false);
+            walking = false;
             ani.SetBool("Attack", true);
         }
+        CheckKeyPresses();
+        transform.LookAt(plTransform);
 	}
+
+    void CheckKeyPresses()
+    {
+        if (walking == true && ani.GetBool("Attack") == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
 }
