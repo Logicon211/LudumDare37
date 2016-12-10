209921d3a35c764bcf21e580dc7e838c299bdabf
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-10 17:26:36 -0400

    Control panel task controller

---
+++ LudumDare37/Assets/Scripts/ControlPanelTaskController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelTaskController : MonoBehaviour {
+
+	private TaskController parentController;
+	private int numberOfControlPanels;
+	private bool taskComplete = false;
+
+	// Use this for initialization
+	void Start () {
+		numberOfControlPanels = GameObject.FindGameObjectsWithTag ("ControlPanel").Length;
+		parentController = GameObject.FindObjectOfType<TaskController> ();
+		checkTaskComplete ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void decrementNumberOfPanels () {
+		if (numberOfControlPanels != 0) {
+			numberOfControlPanels--;
+			Debug.Log ("Decremented, new number of panels: " + numberOfControlPanels);
+			checkTaskComplete ();
+		}
+	}
+
+	private void checkTaskComplete () {
+		if (numberOfControlPanels == 0 && !taskComplete) {
+			taskComplete = true;
+			Debug.Log ("Task complete");
+			// Trigger victory in parent
+		}
+	}
+}
---
+++ LudumDare37/Assets/Scripts/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskController : MonoBehaviour {
+
+	private bool controlPanelTaskComplete = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool getControlPanelTaskComplete () {
+		return controlPanelTaskComplete;
+	}
+
+	public void triggerControlPanelTaskComplete() {
+		controlPanelTaskComplete = true;
+		//Update UI;
+	}
+}
