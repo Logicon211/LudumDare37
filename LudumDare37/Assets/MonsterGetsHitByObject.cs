8a4a7f8c8fa6ede28b9759b2ba5f85d57b2d7882
KingOfBraves <chadifferus@gmail.com>
2016-12-11 00:44:25 -0400

    You can now throw objects at skeletons

---
+++ LudumDare37/Assets/MonsterGetsHitByObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGetsHitByObject : MonoBehaviour {
+
+    MonsterHealth health;
+
+	// Use this for initialization
+	void Start () {
+        health = GetComponentInParent<MonsterHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject g = other.collider.gameObject;
+        if (g.GetComponent<GenericPickupableObject>() != null)
+        {
+            float vel = other.relativeVelocity.magnitude;
+            float mass = g.GetComponent<Rigidbody>().mass;
+
+            if (mass*vel >= 5.0f)
+            {
+                print(mass * vel);
+                health.SetHealth(10);
+            }
+        }
+    }
+}
