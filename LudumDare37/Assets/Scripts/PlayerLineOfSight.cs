c73b7b06f3e557d392fb604a5c91b7f722affad0
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-11 11:49:30 -0400

    Changed input keys, added interaction in range text

--- LudumDare37/Assets/Scripts/PlayerLineOfSight.cs
+++ LudumDare37/Assets/Scripts/PlayerLineOfSight.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLineOfSight : MonoBehaviour {
 
@@ -9,32 +10,44 @@ public class PlayerLineOfSight : MonoBehaviour {
 	bool isObjectInRange;
 	GameObject objectInHand;
 	public LayerMask colliderMask;
+	public Text interactionText;
 
 	public float throwForce = 5;
 
 	// Use this for initialization
 	void Start () {
 		isObjectInRange = false;
+		interactionText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawRay (this.transform.position, this.transform.forward * interactionDistance, Color.magenta);
 
+		IPickupable pickupableObject = null;
+		IInteractable interactableObject = null;
+
 		if (Physics.Raycast (this.transform.position, this.transform.forward, out objectInRange, interactionDistance, colliderMask)) {
 //			Debug.Log ("Object in range: " + objectInRange.collider.name);
+
+			pickupableObject = (IPickupable)objectInRange.collider.gameObject.GetComponent (typeof(IPickupable));
+			interactableObject = (IInteractable)objectInRange.collider.gameObject.GetComponent (typeof(IInteractable));
+
+
 			isObjectInRange = true;
-			if (Input.GetMouseButtonDown (0)) {
-				if (objectInHand != null) {
-					IPickupable interactingWithObject = (IPickupable)objectInRange.collider.gameObject.GetComponent (typeof(IPickupable));
-					if (interactingWithObject != null) {
-						interactingWithObject.Throw (null, this.transform.forward, throwForce);
-						objectInHand = null;
+			if (Input.GetKeyDown(KeyCode.E)) {
+				if (objectInHand == null) {
+					if (interactableObject != null) {
+						interactableObject.Interact ();
 					}
-				} else {
-					IInteractable interactingWithObject = (IInteractable)objectInRange.collider.gameObject.GetComponent (typeof(IInteractable));
-					if (interactingWithObject != null) {
-						interactingWithObject.Interact ();
+				}
+			}
+
+			if (Input.GetMouseButtonDown (1)) {
+				if (objectInHand != null) {
+					if (pickupableObject != null) {
+						pickupableObject.Throw (null, this.transform.forward, throwForce);
+						objectInHand = null;
 					}
 				}
 			}
@@ -57,6 +70,8 @@ public class PlayerLineOfSight : MonoBehaviour {
 				}
 			} 
 		}
+
+		updateInteractionText (interactableObject, pickupableObject);
 	}
 
     public bool IsAbleToPunch()
@@ -65,4 +80,15 @@ public class PlayerLineOfSight : MonoBehaviour {
             return true;
         return false;
     }
+
+	private void updateInteractionText(IInteractable interactable, IPickupable pickupable) {
+		
+		if (interactable != null && objectInHand == null) {
+			interactionText.text = "[E] to interact";
+		} else if (pickupable != null && objectInHand == null) {
+			interactionText.text = "[E] to pickup";
+		} else { 
+			interactionText.text = "";
+		}
+	}
 }
