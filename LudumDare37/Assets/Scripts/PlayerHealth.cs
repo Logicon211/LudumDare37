28decfcb4db0c5ff295a4e86a183f9193222c175
MitchellMaclean <heavyiam@hotmailcom>
2016-12-11 12:26:45 -0400

    Player health script modifications.
    Now plays a sound and displays a red tint when damaged.

--- LudumDare37/Assets/Scripts/PlayerHealth.cs
+++ LudumDare37/Assets/Scripts/PlayerHealth.cs
@@ -7,7 +7,9 @@ public class PlayerHealth : MonoBehaviour {
 
     public int health;
 	public GameObject redDeathOverlayObject;
+	public AudioClip player_hurt;
 
+	AudioSource audio;
 	private Image redDeathImage;
 
     bool dead = false;
@@ -15,6 +17,7 @@ public class PlayerHealth : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		redDeathImage = redDeathOverlayObject.GetComponent<Image> ();
+		audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -28,6 +31,21 @@ public class PlayerHealth : MonoBehaviour {
     public void SetHealth(int damage)
     {
         health -= damage;
+
+		float temp;
+		Color temp1;
+		//temp = redDeathImage.color;
+		temp= (1- (float)health/100);
+		temp1 = redDeathImage.color;
+		temp1.a = temp;
+		//redDeathImage.color.a = temp;
+
+		redDeathImage.enabled = true;
+		redDeathImage.color = temp1;
+		Debug.Log("setting health to" + health);
+		Debug.Log("setting temp to" + temp);
+
+		audio.PlayOneShot(player_hurt, 0.7f);
     }
 
     public int GetHealth()
