46121eacf3512aa4728d8884d4e0f72feb39f778
KingOfBraves <chadifferus@gmail.com>
2016-12-11 15:57:30 -0400

    Who knows maybe the prefgab will go through for once

--- LudumDare37/Assets/Scripts/PlayerAttack.cs
+++ LudumDare37/Assets/Scripts/PlayerAttack.cs
@@ -56,7 +56,7 @@ public class PlayerAttack : MonoBehaviour {
 			Instantiate(hitEffect, new Vector3(target.transform.position.x, target.transform.position.y + 1f, target.transform.position.z), Quaternion.identity);
             monsterHealth = target.GetComponent<MonsterHealth>();
             monsterHealth.SetHealth(attackDamage);
-            audio.PlayOneShot(swingHit, 0.7f);
+            audio.PlayOneShot(swingHit, 1.0f);
             ani.SetBool("Punching", true);
             punchTimer = .2f;
             startTimer = false;
