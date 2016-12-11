a37ece926830d017fd72b6a8d3bb53c823aa6a7b
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-11 00:16:08 -0400

    Light switch works

--- LudumDare37/Assets/Scripts/TaskController.cs
+++ LudumDare37/Assets/Scripts/TaskController.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 
 public class TaskController : MonoBehaviour {
 
+	private bool lightSwitchTaskComplete = false;
 	private bool controlPanelTaskComplete = false;
 	private bool nukeTaskComplete = false;
 
 	public Text taskListTitle;
+	public Text lightSwitchTaskText;
 	public Text controlPanelTaskText;
 	public Text nukeTaskText;
 
 	// Use this for initialization
 	void Start () {
 		taskListTitle.text = "Todo:";
+		lightSwitchTaskText.text = "- Hit the lights";
 		controlPanelTaskText.text = "- Activate all control panels";
 		nukeTaskText.text = "- Load all nukes into engine";
 	}
@@ -32,7 +35,6 @@ public class TaskController : MonoBehaviour {
 		controlPanelTaskComplete = true;
 		controlPanelTaskText.supportRichText = true;
 		controlPanelTaskText.text = "<color=#292929ff>- Activate all control panels</color>";
-		//Update UI;
 	}
 
 	public bool getNukeTaskComplete () {
@@ -43,6 +45,15 @@ public class TaskController : MonoBehaviour {
 		nukeTaskComplete = true;
 		nukeTaskText.supportRichText = true;
 		nukeTaskText.text = "<color=#292929ff>- Load all nukes into engine</color>";
-		//Update UI;
+	}
+
+	public bool getLightSwitchTaskComplete () {
+		return controlPanelTaskComplete;
+	}
+
+	public void triggerLightSwitchTaskComplete() {
+		lightSwitchTaskComplete = true;
+		lightSwitchTaskText.supportRichText = true;
+		lightSwitchTaskText.text = "<color=#292929ff>- Hit the lights</color>";
 	}
 }
