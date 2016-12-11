52042e4154269d97369a1ae5a4e11ab4d90912ee
BroderickBeaman <broderick.beaman@unb.ca>
2016-12-10 21:46:21 -0400

    Task UI

--- LudumDare37/Assets/Scripts/TaskController.cs
+++ LudumDare37/Assets/Scripts/TaskController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskController : MonoBehaviour {
 
 	private bool controlPanelTaskComplete = false;
 	private bool nukeTaskComplete = false;
 
+	public Text taskListTitle;
+	public Text controlPanelTaskText;
+	public Text nukeTaskText;
+
 	// Use this for initialization
 	void Start () {
-		
+		taskListTitle.text = "Todo:";
+		controlPanelTaskText.text = "- Activate all control panels";
+		nukeTaskText.text = "- Load all nukes into engine";
 	}
 	
 	// Update is called once per frame
@@ -23,6 +30,8 @@ public class TaskController : MonoBehaviour {
 
 	public void triggerControlPanelTaskComplete() {
 		controlPanelTaskComplete = true;
+		controlPanelTaskText.supportRichText = true;
+		controlPanelTaskText.text = "<color=#292929ff>- Activate all control panels</color>";
 		//Update UI;
 	}
 
@@ -32,6 +41,8 @@ public class TaskController : MonoBehaviour {
 
 	public void triggerNukeTaskComplete() {
 		nukeTaskComplete = true;
+		nukeTaskText.supportRichText = true;
+		nukeTaskText.text = "<color=#292929ff>- Load all nukes into engine</color>";
 		//Update UI;
 	}
 }
